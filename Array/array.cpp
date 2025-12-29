a52bfb140e23d9c874b651e4017ccd4ba9531b99
nevil811 <nevilbarick69@gmail.com>
2025-12-29 20:37:55 +0530

    Arrays

--- Array/array.cpp
+++ Array/array.cpp
@@ -1,7 +1,12 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-  int marks [5] ={99,100,54,36,88};
-  cout<<marks<<endl;
-  return 0;
+
+int main() {
+    int marks[5] = {99, 100, 54, 36, 88};
+
+    for (int i = 0; i < 5; i++) {
+        cout << marks[i] << " ";
+    }
+
+    return 0;
 }

ac6118f2d6dc5b99f4963494ccbe76102283a6ac
nevil811 <nevilbarick69@gmail.com>
2026-01-04 21:09:07 +0530

    Kadane's Algo - Sum of Max subarray

--- Kadane's Algorithm/sum_subarray.cpp
+++ Kadane's Algorithm/sum_subarray.cpp
@@ -18,26 +18,26 @@
 //  }
 
 
-#include <iostream>
-#include <vector>
-#include <climits>
-using namespace std;
-int main()
-{
+// #include <iostream>
+// #include <vector>
+// #include <climits>
+// using namespace std;
+// int main()
+// {
 
-  int n = 5;
-  int arr[] = {1, 2, 3, 4, 5};
-  int maxSum = INT_MIN;
- int currSum=0;
- for(int st=0;st<n;st++){
-  currSum+=st;
-  maxSum=max(currSum,maxSum);
-if(currSum<0){
-  currSum=0;
-} 
-cout<<"Sum of is "<<maxSum<<endl;
-} 
+//   int n = 5;
+//   int arr[] = {1, 2, 3, 4, 5};
+//   int maxSum = INT_MIN;
+//  int currSum=0;
+//  for(int st=0;st<n;st++){
+//   currSum+=st;
+//   maxSum=max(currSum,maxSum);
+// if(currSum<0){
+//   currSum=0;
+// } 
+// cout<<"Sum of is "<<maxSum<<endl;
+// } 
  
 
-  return 0;
-}
+//   return 0;
+// }
